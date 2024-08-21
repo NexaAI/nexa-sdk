4205236733bd6bec10d8843d0ac2f80c86e4f474
Yu Xing <xyyimian@gmail.com>
2024-08-21 23:26:31 +0000

    use engine interface to test

--- tests/test_vlm.py
+++ tests/test_vlm.py
@@ -1,32 +1,17 @@
 from nexa.gguf import NexaVLMInference
 import tempfile
+from .utils import download_model
 
 def test_image_generation():
     with tempfile.TemporaryDirectory() as temp_dir:
         model = NexaVLMInference(
             model_path="nanollava",
         )
-        output = model.create_chat_completion(
-            messages=[
-                {
-                    "role": "system",
-                    "content": "You are an assistant who perfectly describes images.",
-                },
-                {
-                    "role": "user",
-                    "content": [
-                        {"type": "text", "text": "What's in this image?"},
-                        {
-                            "type": "image_url",
-                            "image_url": {
-                                "url": "https://www.wikipedia.org/portal/wikipedia.org/assets/img/Wikipedia-logo-v2.png"
-                            },
-                        },
-                    ],
-                },
-            ],
-            stream=True,
+        image_path = download_model(
+            "https://www.wikipedia.org/portal/wikipedia.org/assets/img/Wikipedia-logo-v2.png",
+            temp_dir,
         )
+        output = model._chat("what's in this image?", image_path)
         for chunk in output:
             delta = chunk["choices"][0]["delta"]
             if "role" in delta:
