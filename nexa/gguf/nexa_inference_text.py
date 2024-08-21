9a78395f6d8816afc9a0a8643463e8c49fe4e60c
Yu Xing <xyyimian@gmail.com>
2024-08-21 23:27:52 +0000

    revert cursor and use interface

--- nexa/gguf/nexa_inference_text.py
+++ nexa/gguf/nexa_inference_text.py
@@ -98,7 +98,7 @@ class NexaTextInference:
         """
         return self.model.create_embedding(input)
 
-    # @SpinningCursorAnimation()
+    @SpinningCursorAnimation()
     def _load_model(self):
         logging.debug(f"Loading model from {self.downloaded_path}")
         start_time = time.time()
