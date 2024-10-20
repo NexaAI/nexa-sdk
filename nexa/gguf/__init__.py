a6bd9249c78851f8d6777d204e3813754e8bad0b
Paul Zhu <paulzhuzx@gmail.com>
2024-10-19 21:58:12 -0700

    Merge with main

--- nexa/gguf/__init__.py
+++ nexa/gguf/__init__.py
@@ -2,7 +2,6 @@ from .nexa_inference_image import NexaImageInference
 from .nexa_inference_text import NexaTextInference
 from .nexa_inference_vlm import NexaVLMInference
 from .nexa_inference_voice import NexaVoiceInference
-from .nexa_inference_tts import NexaTTSInference
 
 __all__ = [
     "NexaImageInference",
