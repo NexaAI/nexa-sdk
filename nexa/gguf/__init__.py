a353a8bbde760db8f49b792dad87195938671535
Paul Zhu <paulzhuzx@gmail.com>
2024-10-22 21:56:05 -0700

    Update init

--- nexa/gguf/__init__.py
+++ nexa/gguf/__init__.py
@@ -2,10 +2,12 @@ from .nexa_inference_image import NexaImageInference
 from .nexa_inference_text import NexaTextInference
 from .nexa_inference_vlm import NexaVLMInference
 from .nexa_inference_voice import NexaVoiceInference
+from .nexa_inference_tts import NexaTTSInference
 
 __all__ = [
     "NexaImageInference",
     "NexaTextInference",
     "NexaVLMInference",
     "NexaVoiceInference",
+    "NexaTTSInference",
 ]
