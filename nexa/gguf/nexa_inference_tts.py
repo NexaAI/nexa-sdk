429c1f81f65726aa35e53dc0e791f395a1f7ba8e
Paul Zhu <paulzhuzx@gmail.com>
2024-10-25 01:38:27 -0700

    Use pull_model and create test file for nexa tts inference

--- nexa/gguf/nexa_inference_tts.py
+++ nexa/gguf/nexa_inference_tts.py
@@ -4,6 +4,7 @@ import logging
 import numpy as np
 import time
 from .bark import bark_cpp
+from nexa.general import pull_model
 
 class NexaTTSInference:
     """
@@ -22,8 +23,12 @@ class NexaTTSInference:
         verbosity (int): Verbosity level for the Bark model. Defaults to 0.
     """
     
-    def __init__(self, model_path, n_threads=1, seed=0, sampling_rate=24000, verbosity=0, **kwargs):
+    def __init__(self, model_path=None, local_path=None, n_threads=1, seed=0, sampling_rate=24000, verbosity=0, **kwargs):
+        if model_path is None and local_path is None:
+            raise ValueError("Either model_path or local_path must be provided.")
+            
         self.model_path = model_path
+        self.downloaded_path = local_path
         self.n_threads = n_threads
         self.seed = seed
         self.sampling_rate = sampling_rate
@@ -33,15 +38,26 @@ class NexaTTSInference:
         }
         self.params.update(kwargs)
         self.context = None
+
+        if self.downloaded_path is None:
+            self.downloaded_path, _ = pull_model(self.model_path, **kwargs)
+
+        if self.downloaded_path is None:
+            logging.error(
+                f"Model ({model_path}) is not applicable. Please refer to our docs for proper usage.",
+                exc_info=True,
+            )
+            exit(1)
+
         self._load_model()
 
     def _load_model(self):
-        logging.debug(f"Loading model from {self.model_path}")
+        logging.debug(f"Loading model from {self.downloaded_path}")
         try:
             params = bark_cpp.bark_context_default_params()
             params.sample_rate = self.sampling_rate
             params.verbosity = self.verbosity
-            c_model_path = ctypes.c_char_p(self.model_path.encode('utf-8'))
+            c_model_path = ctypes.c_char_p(self.downloaded_path.encode('utf-8'))
             c_seed = ctypes.c_void_p(self.seed)
             self.context = bark_cpp.bark_load_model(c_model_path, params, c_seed)
             if not self.context:
@@ -50,6 +66,7 @@ class NexaTTSInference:
         except Exception as e:
             logging.error(f"Error loading model: {e}")
             raise
+
 
     def run(self):
         while True:
@@ -63,6 +80,7 @@ class NexaTTSInference:
                 break
             except Exception as e:
                 logging.error(f"Error during audio generation: {e}", exc_info=True)
+
 
     def audio_generation(self, user_input):
         """
@@ -85,12 +103,14 @@ class NexaTTSInference:
         
         return np.ctypeslib.as_array(audio_data, shape=(audio_size,))
 
+
     def _save_audio(self, audio_data, sampling_rate, output_path):
         os.makedirs(output_path, exist_ok=True)
         file_name = f"audio_{int(time.time())}.wav"
         file_path = os.path.join(output_path, file_name)
         import soundfile as sf
         sf.write(file_path, audio_data, sampling_rate)
+
 
     def get_load_time(self):
         """
@@ -101,6 +121,7 @@ class NexaTTSInference:
         """
         return bark_cpp.bark_get_load_time(self.context)
 
+
     def get_eval_time(self):
         """
         Get the time taken to evaluate (generate audio).
@@ -110,11 +131,13 @@ class NexaTTSInference:
         """
         return bark_cpp.bark_get_eval_time(self.context)
 
+
     def reset_statistics(self):
         """
         Reset the internal statistics of the Bark context.
         """
         bark_cpp.bark_reset_statistics(self.context)
+
 
     def __del__(self):
         """
@@ -127,7 +150,7 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description="Run text to speech generation with Bark model")
-    parser.add_argument("model_path", type=str, help="Path to the Bark model file")
+    parser.add_argument("model_path", type=str, help="Path or identifier for the model in Nexa Model Hub")
     parser.add_argument("-o", "--output_dir", type=str, default="tts", help="Output directory for tts")
     parser.add_argument("-r", "--sampling_rate", type=int, default=24000, help="Sampling rate for audio processing")
     parser.add_argument("-t", "--n_threads", type=int, default=1, help="Number of threads to use for processing")
@@ -137,5 +160,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
     kwargs = {k: v for k, v in vars(args).items() if k not in ["model_path", "n_threads", "seed", "sampling_rate", "verbosity"]}
     
-    inference = NexaTTSInference(args.model_path, args.n_threads, args.seed, args.sampling_rate, args.verbosity, **kwargs)
+    inference = NexaTTSInference(args.model_path, n_threads=args.n_threads, seed=args.seed, 
+                                sampling_rate=args.sampling_rate, verbosity=args.verbosity, **kwargs)
     inference.run()
