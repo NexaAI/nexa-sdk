2519e1596c300021215930a0641ac8524667fdc4
Zack Zhiyuan Li <zhiyuanli0718@gmail.com>
2024-11-05 21:19:38 +0000

    use q4_0 for audioLM

--- nexa/constants.py
+++ nexa/constants.py
@@ -110,7 +110,7 @@ NEXA_RUN_MODEL_MAP_VLM = {
 }
 
 NEXA_RUN_MODEL_MAP_AUDIO_LM = {
-    "qwen2audio": "Qwen2-Audio-7.8B-Instruct:model-fp16",
+    "qwen2audio": "Qwen2-Audio-7.8B-Instruct:model-q4_0",
     "qwen2audio:fp16": "Qwen2-Audio-7.8B-Instruct:model-fp16",
     "qwen2audio:q4_0": "Qwen2-Audio-7.8B-Instruct:model-q4_0",
     "omniaudio": "Octo-omni-audio:model-fp16",
@@ -183,7 +183,7 @@ NEXA_RUN_OMNI_VLM_MAP = {
 }
 
 NEXA_RUN_AUDIO_LM_PROJECTOR_MAP = {
-    "qwen2audio": "Qwen2-Audio-7.8B-Instruct:projector-fp16",
+    "qwen2audio": "Qwen2-Audio-7.8B-Instruct:projector-q4_0",
     "qwen2audio:fp16": "Qwen2-Audio-7.8B-Instruct:projector-fp16",
     "qwen2audio:q4_0": "Qwen2-Audio-7.8B-Instruct:projector-q4_0",
     "omniaudio": "Octo-omni-audio:projector-fp16",
