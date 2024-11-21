551f50b45565da6decfb2deb12909d6426ca7b1e
Te993 <3923106166@qq.com>
2024-11-21 10:44:25 +0800

    add android

---
+++ android/llama.android/settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LlamaAndroid"
+//include(":app")
+include(":llama")
+include(":app-java")
