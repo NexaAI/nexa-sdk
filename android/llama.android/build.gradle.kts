26f5a5a5a5457506ad556d5b0c16b0738012573c
T <3923106166@qq.com>
2024-11-29 09:21:25 +0800

    [Android]Add android sdk and sample app (#262)
    * add android
    
    * update readme
    
    * add android workflow
    
    * update android workflow
    
    * updaye android ignore and upload some files
    
    * updaye android ignore and upload some files
    
    * fix step - Rename and upload AAR
    
    * fix step - Upload AAR as an artifact
    
    * add job to upload arr to assets
    
    * update build aar pipeline

---
+++ android/llama.android/build.gradle.kts
@@ -0,0 +1,13 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.library") version "8.2.0" apply false
+}
+buildscript {
+    val kotlin_version by extra("1.9.20")
+
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+    }
+}
