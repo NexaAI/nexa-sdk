7ea07fc50096420188e76d3dd26798e5387bd22b
Paul Zhu <35409183+vinovo@users.noreply.github.com>
2025-08-08 17:05:22 -0700

    feat: fix qwen3 friendly name repo and add gpt-oss friendly name (#152)

--- runner/internal/config/model_mapping.go
+++ runner/internal/config/model_mapping.go
@@ -1,11 +1,13 @@
 package config
 
 var modelMapping = map[string]string{
-	"qwen3":     "Qwen/Qwen3-4B-GGUF",
-	"qwen2vl":   "ggml-org/Qwen2-VL-2B-Instruct-GGUF",
-	"qwen2.5vl": "Qwen/Qwen2.5-VL-3B-Instruct",
-	"gemma3":    "ggml-org/gemma-3-4b-it-GGUF",
-	"smolvlm":   "ggml-org/SmolVLM-500M-Instruct-GGUF",
+	"qwen3":       "NexaAI/Qwen3-4B-GGUF",
+	"qwen2vl":     "ggml-org/Qwen2-VL-2B-Instruct-GGUF",
+	"qwen2.5vl":   "Qwen/Qwen2.5-VL-3B-Instruct",
+	"gemma3":      "ggml-org/gemma-3-4b-it-GGUF",
+	"smolvlm":     "ggml-org/SmolVLM-500M-Instruct-GGUF",
+	"gpt-oss":     "NexaAI/gpt-oss-20b-GGUF",
+	"gpt-oss-mlx": "NexaAI/gpt-oss-20b-MLX-4bit",
 }
 
 func GetModelMapping(shortcut string) (string, bool) {
