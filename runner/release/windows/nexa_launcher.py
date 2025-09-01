93ce0717b8797745a853264e79b64dff6a8a99e9
lxsong <672528307@qq.com>
2025-09-01 22:58:25 +0800

    feat: build windows launcher using golang

--- runner/release/windows/nexa_launcher.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-# Launch PowerShell with the -NoProfile flag which is generally faster to start
-# Use -NoExit to keep PowerShell open after running the command
-# Use -Command to specify the command to run
-subprocess.Popen(
-    ["powershell", "-NoProfile", "-NoExit", "-Command", "nexa"],
-    creationflags=subprocess.CREATE_NEW_CONSOLE
-)
