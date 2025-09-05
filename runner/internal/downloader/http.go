8f624edf2d5024f49002ec8a53798e310ab42cb8
Mengsheng Wu <mengshengwu@outlook.com>
2025-09-05 16:19:00 +0800

    refactor(downloader): update HTTPDownloader to use fasthttp.Client directly and comment out unimplemented download logic

--- runner/internal/downloader/http.go
+++ runner/internal/downloader/http.go
@@ -3,24 +3,21 @@ package downloader
 import (
 	"context"
 	"fmt"
-	"log/slog"
 	"net/url"
 	"os"
-	"path/filepath"
-	"sync"
 
 	"github.com/valyala/fasthttp"
 )
 
 type HTTPDownloader struct {
-	client fasthttp.Client
+	fasthttp.Client
 
 	token string
 }
 
 func NewDownloader() *HTTPDownloader {
 	return &HTTPDownloader{
-		client: fasthttp.Client{
+		Client: fasthttp.Client{
 			NoDefaultUserAgentHeader:  true,
 			MaxIdemponentCallAttempts: 3,
 			ReadBufferSize:            64 * 1024,
@@ -34,98 +31,82 @@ func (d *HTTPDownloader) SetToken(token string) {
 }
 
 func (d *HTTPDownloader) Download(ctx context.Context, url, outputPath string, progress chan int64) error {
-	slog.Debug("Download", "url", url, "outputPath", outputPath)
+	// 	slog.Debug("Download", "url", url, "outputPath", outputPath)
 
-	url, err := d.handleRedirect(url, 3)
-	if err != nil {
-		return fmt.Errorf("failed to handle redirect: %v", err)
-	}
+	// 	url, err := d.handleRedirect(url, 3)
+	// 	if err != nil {
+	// 		return fmt.Errorf("failed to handle redirect: %v", err)
+	// 	}
 
-	contentLength, err := d.getFilesSize(url)
-	if err != nil {
-		return fmt.Errorf("failed to get file size: %v", err)
-	}
+	// 	contentLength, err := d.GetFilesSize(url)
+	// 	if err != nil {
+	// 		return fmt.Errorf("failed to get file size: %v", err)
+	// 	}
 
-	{ // pre-allocate file
-		if err := os.MkdirAll(filepath.Dir(outputPath), 0o755); err != nil {
-			return fmt.Errorf("failed to create directory: %v", err)
-		}
-		file, err := os.OpenFile(outputPath, os.O_CREATE|os.O_WRONLY, 0o644)
-		if err != nil {
-			return fmt.Errorf("failed to create file: %v", err)
-		}
-		err = file.Truncate(contentLength)
-		file.Close()
-		if err != nil {
-			return fmt.Errorf("failed to truncate file: %v", err)
-		}
-	}
+	// 	{ // pre-allocate file
+	// 		if err := os.MkdirAll(filepath.Dir(outputPath), 0o755); err != nil {
+	// 			return fmt.Errorf("failed to create directory: %v", err)
+	// 		}
+	// 		file, err := os.OpenFile(outputPath, os.O_CREATE|os.O_WRONLY, 0o644)
+	// 		if err != nil {
+	// 			return fmt.Errorf("failed to create file: %v", err)
+	// 		}
+	// 		err = file.Truncate(contentLength)
+	// 		file.Close()
+	// 		if err != nil {
+	// 			return fmt.Errorf("failed to truncate file: %v", err)
+	// 		}
+	// 	}
 
-	var wg sync.WaitGroup
-	sem := make(chan struct{}, 8)
-	cancelCtx, cancel := context.WithCancel(ctx)
-	defer cancel()
-	chunkSize := calcChunkSize(contentLength)
-	errCh := make(chan error, 1)
+	// 	var wg sync.WaitGroup
+	// 	sem := make(chan struct{}, 8)
+	// 	cancelCtx, cancel := context.WithCancel(ctx)
+	// 	defer cancel()
+	// 	chunkSize := calcChunkSize(contentLength)
+	// 	errCh := make(chan error, 1)
 
-	for start := int64(0); start < contentLength; start += chunkSize {
-		slog.Info("Scheduling chunk", "start", start, "contentLength", contentLength)
-		end := start + chunkSize - 1
-		if end >= contentLength {
-			end = contentLength - 1
-		}
+	// 	for start := int64(0); start < contentLength; start += chunkSize {
+	// 		slog.Info("Scheduling chunk", "start", start, "contentLength", contentLength)
+	// 		end := start + chunkSize - 1
+	// 		if end >= contentLength {
+	// 			end = contentLength - 1
+	// 		}
 
-		select {
-		case sem <- struct{}{}:
-			wg.Add(1)
+	// 		select {
+	// 		case sem <- struct{}{}:
+	// 			wg.Add(1)
 
-			go func(start, end int64) {
-				defer wg.Done()
-				defer func() { <-sem }()
-				if err := d.DownloadChunk(cancelCtx, url, outputPath, start, end, progress); err != nil {
-					select { // non-blocking send error
-					case errCh <- err:
-						slog.Error("Download chunk failed", "start", start, "end", end, "error", err)
-					default:
-					}
-					cancel()
-				}
-			}(start, end)
+	// 			go func(start, end int64) {
+	// 				defer wg.Done()
+	// 				defer func() { <-sem }()
+	// 				if err := d.DownloadChunk(cancelCtx, url, outputPath, start, end, progress); err != nil {
+	// 					select { // non-blocking send error
+	// 					case errCh <- err:
+	// 						slog.Error("Download chunk failed", "start", start, "end", end, "error", err)
+	// 					default:
+	// 					}
+	// 					cancel()
+	// 				}
+	// 			}(start, end)
 
-		case <-cancelCtx.Done():
-			wg.Wait()
-			return fmt.Errorf("download canceled: %v", cancelCtx.Err())
-		}
-	}
-	wg.Wait()
+	// 		case <-cancelCtx.Done():
+	// 			wg.Wait()
+	// 			return fmt.Errorf("download canceled: %v", cancelCtx.Err())
+	// 		}
+	// 	}
+	// 	wg.Wait()
 
-	select {
-	case err := <-errCh:
-		os.Remove(outputPath)
-		return err
-	default:
-		return nil
-	}
-}
-
-func (d *HTTPDownloader) getFilesSize(url string) (int64, error) {
-	req := fasthttp.AcquireRequest()
-	resp := fasthttp.AcquireResponse()
-	defer fasthttp.ReleaseRequest(req)
-	defer fasthttp.ReleaseResponse(resp)
-
-	req.SetRequestURI(url)
-	req.Header.SetMethod(fasthttp.MethodHead)
-	if d.token != "" {
-		req.Header.Set("Authorization", "Bearer "+d.token)
-	}
-	req.Header.Set("Accept-Encoding", "identity")
-
-	if err := d.client.Do(req, resp); err != nil {
-		return -1, err
-	}
-
-	return int64(resp.Header.ContentLength()), nil
+	// select {
+	// case err := <-errCh:
+	//
+	//	os.Remove(outputPath)
+	//	return err
+	//
+	// default:
+	//
+	//		return nil
+	//	}
+	panic("unimplemented")
 }
 
 func (d *HTTPDownloader) handleRedirect(url string, maxRedirect int) (string, error) {
@@ -146,7 +127,7 @@ func (d *HTTPDownloader) handleRedirect(url string, maxRedirect int) (string, er
 			req.Header.Set("Authorization", "Bearer "+d.token)
 		}
 
-		if err := d.client.Do(req, resp); err != nil {
+		if err := d.Client.Do(req, resp); err != nil {
 			return "", fmt.Errorf("request failed: %w", err)
 		}
 
@@ -217,7 +198,7 @@ func (d *HTTPDownloader) DownloadChunk(_ context.Context, url, outputPath string
 	}
 	req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", start, end))
 
-	if err := d.client.Do(req, resp); err != nil {
+	if err := d.Client.Do(req, resp); err != nil {
 		return err
 	}
 
