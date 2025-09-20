f6932b2ef43d28871df38127345291f520c3697b
RemiliaForever <remilia@koumakan.cc>
2025-09-20 11:54:10 +0800

    refactor(cli): refactor migrate design

--- runner/cmd/nexa-cli/migrate.go
+++ runner/cmd/nexa-cli/migrate.go
@@ -1,19 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"errors"
 	"fmt"
 	"log/slog"
 	"os"
 	"path/filepath"
-
-	"github.com/spf13/cobra"
+	"strings"
 
 	"github.com/NexaAI/nexa-sdk/runner/internal/model_hub"
 	"github.com/NexaAI/nexa-sdk/runner/internal/render"
 	"github.com/NexaAI/nexa-sdk/runner/internal/store"
 	"github.com/NexaAI/nexa-sdk/runner/internal/types"
+	"github.com/jedib0t/go-pretty/v6/table"
 )
 
 func checkMigrate() error {
@@ -27,139 +28,178 @@ func checkMigrate() error {
 	}
 
 	if currentVersion != lastVersion {
-		fmt.Println(render.GetTheme().Warning.Sprintf(`
-A new version of Nexa CLI is detected. Please run "nexa migrate" to migrate your models.
-Use "nexa migrate --help" to see more options.
-		`))
-		return errors.New("need migrate")
+		// skip when no model cached
+		models, err := store.Get().List()
+		if err != nil {
+			return err
+		}
+		if len(models) == 0 {
+			return finishMigrate()
+		}
+
+		fmt.Print(render.GetTheme().Warning.Sprintf("A new version of Nexa CLI is detected. Start migrate now ? [Y/n] "))
+		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		input = strings.TrimSpace(input)
+		input = strings.ToLower(input)
+		if input == "y" || input == "" {
+			if err := startMigrate(); err != nil {
+				return err
+			}
+			finishMigrate()
+		} else {
+			fmt.Println()
+			fmt.Println(render.GetTheme().Warning.Sprintf("Miration cancelled. You can not use old models until migrate completed."))
+			fmt.Println(render.GetTheme().Warning.Sprintf("Or you can run `nexa clean` to remove all old models."))
+			fmt.Println()
+			return errors.New("canceld")
+		}
 	}
 
 	return nil
 }
 
-func migrate() *cobra.Command {
-	migrateCmd := &cobra.Command{
-		Use:   "migrate",
-		Short: "Migrate models from previous versions",
+func finishMigrate() error {
+	err := os.WriteFile(filepath.Join(store.Get().DataPath(), "last_version"), []byte(Version), 0o600)
+	if err != nil {
+		fmt.Println(render.GetTheme().Error.Sprintf("Failed to write last_version: %v", err))
+		return err
+	}
+	fmt.Println(render.GetTheme().Success.Sprintf("Migration completed."))
+	return nil
+}
+
+type MigrateResult struct {
+	ModelName string
+	Status    string
+}
+
+const (
+	StatusSkip    = "skip"
+	StatusSuccess = "success"
+	StatusFailed  = "failed"
+)
+
+func startMigrate() error {
+
+	s := store.Get()
+
+	models, err := s.List()
+	if err != nil {
+		fmt.Println(render.GetTheme().Error.Sprintf("Failed to get model list: %v", err))
+		return err
 	}
 
-	skip := migrateCmd.Flags().Bool("skip", false, "force skip migration")
-	deleteOnly := migrateCmd.Flags().Bool("delete-only", false, "delete outdated models instead of migrating")
+	result := make([]MigrateResult, len(models))
 
-	migrateCmd.Run = func(cmd *cobra.Command, args []string) {
-		if *skip {
-			err := os.WriteFile(filepath.Join(store.Get().DataPath(), "last_version"), []byte(Version), 0o600)
-			if err != nil {
-				fmt.Println(render.GetTheme().Error.Sprintf("Failed to write last_version: %v", err))
-			}
-			fmt.Println(render.GetTheme().Warning.Sprintf("Migration skipped. Please make sure your models are compatible with the current version."))
-			return
-		}
+	for i, model := range models {
+		// check if model has update
+		spin := render.NewSpinner("Checking model " + model.Name)
+		spin.Start()
+		files, hmf, err := model_hub.ModelInfo(context.Background(), model.Name)
+		spin.Stop()
 
-		s := store.Get()
-
-		models, err := s.List()
 		if err != nil {
-			fmt.Println(render.GetTheme().Error.Sprintf("Failed to get model list: %v", err))
-			return
+			fmt.Println(render.GetTheme().Error.Sprintf("Failed to get model info for %s: %v", model.Name, err))
+			return err
 		}
 
-		for _, model := range models {
-			// check if model has update
-			spin := render.NewSpinner("Checking model " + model.Name)
-			spin.Start()
-			files, hmf, err := model_hub.ModelInfo(context.Background(), model.Name)
-			spin.Stop()
+		if hmf == nil || model.MinSDKVersion == hmf.MinSDKVersion {
+			result[i] = MigrateResult{ModelName: model.Name, Status: StatusSkip}
+			continue
+		}
 
+		if !isValidVersion(hmf.MinSDKVersion) {
+			fmt.Println(render.GetTheme().Error.Sprintf("Model %s requires NexaSDK CLI version %s or higher. Please upgrade your NexaSDK CLI.", model.Name, hmf.MinSDKVersion))
+			return err
+		}
+
+		// start migrate
+		if err := s.Remove(model.Name); err != nil {
+			fmt.Println(render.GetTheme().Error.Sprintf("Failed to delete model %s: %v", model.Name, err))
+			return err
+		}
+
+		fmt.Println(render.GetTheme().Info.Sprintf("Start pull model %s", model.Name))
+
+		// TODO: support multi quant
+		if false {
+			// newManifest, err := chooseQuantFiles(*mf)
+			// if err != nil {
+			// 	return
+			// }
+			// pgCh, errCh := s.PullExtraQuant(context.TODO(), *mf, *newManifest)
+			// bar := render.NewProgressBar(newManifest.GetSize()-mf.GetSize(), "downloading")
+			//
+			// for pg := range pgCh {
+			// 	bar.Set(pg.TotalDownloaded)
+			// }
+			// bar.Exit()
+			//
+			// for err := range errCh {
+			// 	bar.Clear()
+			// 	fmt.Println(render.GetTheme().Error.Sprintf("Error: %s", err))
+			// }
+		} else {
+			var manifest types.ModelManifest
+
+			manifest.ModelName = hmf.ModelName
+			manifest.PluginId = hmf.PluginId
+			manifest.ModelType = hmf.ModelType
+			manifest.MinSDKVersion = hmf.MinSDKVersion
+
+			if manifest.ModelName == "" {
+				manifest.ModelName = model.ModelName
+			}
+			if manifest.PluginId == "" {
+				manifest.PluginId = model.PluginId
+			}
+			if manifest.ModelType == "" {
+				manifest.ModelType = model.ModelType
+			}
+
+			err := chooseFiles(model.Name, files, &manifest)
 			if err != nil {
-				fmt.Println(render.GetTheme().Error.Sprintf("Failed to get model info for %s: %v", model.Name, err))
-				return
+				fmt.Println(render.GetTheme().Error.Sprintf("Error: %s", err))
+				return err
 			}
 
-			if hmf == nil || model.MinSDKVersion == hmf.MinSDKVersion {
-				continue
+			pgCh, errCh := s.Pull(context.TODO(), manifest)
+			bar := render.NewProgressBar(manifest.GetSize(), "downloading")
+
+			for pg := range pgCh {
+				bar.Set(pg.TotalDownloaded)
 			}
+			bar.Exit()
+			result[i] = MigrateResult{ModelName: model.Name, Status: StatusSuccess}
 
-			if !isValidVersion(hmf.MinSDKVersion) {
-				fmt.Println(render.GetTheme().Error.Sprintf("Model %s requires NexaSDK CLI version %s or higher. Please upgrade your NexaSDK CLI.", model.Name, hmf.MinSDKVersion))
-				return
-			}
-
-			// start migrate
-			if err := s.Remove(model.Name); err != nil {
-				fmt.Println(render.GetTheme().Error.Sprintf("Failed to delete model %s: %v", model.Name, err))
-				return
-			}
-
-			if *deleteOnly {
-				continue
-			}
-
-			fmt.Println(render.GetTheme().Info.Sprintf("Start pull model %s", model.Name))
-
-			// TODO: support multi quant
-			if false {
-				// newManifest, err := chooseQuantFiles(*mf)
-				// if err != nil {
-				// 	return
-				// }
-				// pgCh, errCh := s.PullExtraQuant(context.TODO(), *mf, *newManifest)
-				// bar := render.NewProgressBar(newManifest.GetSize()-mf.GetSize(), "downloading")
-				//
-				// for pg := range pgCh {
-				// 	bar.Set(pg.TotalDownloaded)
-				// }
-				// bar.Exit()
-				//
-				// for err := range errCh {
-				// 	bar.Clear()
-				// 	fmt.Println(render.GetTheme().Error.Sprintf("Error: %s", err))
-				// }
-			} else {
-				var manifest types.ModelManifest
-
-				manifest.ModelName = hmf.ModelName
-				manifest.PluginId = hmf.PluginId
-				manifest.ModelType = hmf.ModelType
-				manifest.MinSDKVersion = hmf.MinSDKVersion
-
-				if manifest.ModelName == "" {
-					manifest.ModelName = model.ModelName
-				}
-				if manifest.PluginId == "" {
-					manifest.PluginId = model.PluginId
-				}
-				if manifest.ModelType == "" {
-					manifest.ModelType = model.ModelType
-				}
-
-				err := chooseFiles(model.Name, files, &manifest)
-				if err != nil {
-					fmt.Println(render.GetTheme().Error.Sprintf("Error: %s", err))
-					return
-				}
-
-				pgCh, errCh := s.Pull(context.TODO(), manifest)
-				bar := render.NewProgressBar(manifest.GetSize(), "downloading")
-
-				for pg := range pgCh {
-					bar.Set(pg.TotalDownloaded)
-				}
-				bar.Exit()
-
-				for err := range errCh {
-					bar.Clear()
-					fmt.Println(render.GetTheme().Error.Sprintf("Error: %s", err))
-				}
+			for err := range errCh {
+				bar.Clear()
+				fmt.Println(render.GetTheme().Error.Sprintf("Error: %s", err))
+				result[i] = MigrateResult{ModelName: model.Name, Status: StatusFailed}
 			}
 		}
-
-		err = os.WriteFile(filepath.Join(store.Get().DataPath(), "last_version"), []byte(Version), 0o600)
-		if err != nil {
-			fmt.Println(render.GetTheme().Error.Sprintf("Failed to write last_version: %v", err))
-			return
-		}
-		fmt.Println(render.GetTheme().Success.Sprintf("Migration completed successfully."))
 	}
-	return migrateCmd
+
+	hasError := false
+	tw := table.NewWriter()
+	tw.SetOutputMirror(os.Stdout)
+	tw.SetStyle(table.StyleLight)
+	tw.AppendHeader(table.Row{render.GetTheme().Success.Sprintf("Migratetion Result")})
+	for _, r := range result {
+		switch r.Status {
+		case StatusSkip:
+			tw.AppendRow(table.Row{r.ModelName, render.GetTheme().Info.Sprintf("skipped (already update)")})
+		case StatusSuccess:
+			tw.AppendRow(table.Row{r.ModelName, render.GetTheme().Success.Sprintf("success")})
+		case StatusFailed:
+			hasError = true
+			tw.AppendRow(table.Row{r.ModelName, render.GetTheme().Warning.Sprintf("failed")})
+		}
+	}
+	tw.Render()
+	if hasError {
+		fmt.Println(render.GetTheme().Warning.Sprintf("Some models failed to migrate. You need download it manually."))
+	}
+
+	return nil
 }
