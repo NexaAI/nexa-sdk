26f2d8554d7ed426e1070c91086342fcf2aa3aaf
Paul Zhu <paulzhuzx@gmail.com>
2025-10-08 23:18:50 +0000

    feat: remove version numbe from nexa version

--- runner/cmd/nexa-cli/version.go
+++ runner/cmd/nexa-cli/version.go
@@ -6,8 +6,6 @@ import (
 
 	goversion "github.com/hashicorp/go-version"
 	"github.com/spf13/cobra"
-
-	nexa_sdk "github.com/NexaAI/nexa-sdk/runner/nexa-sdk"
 )
 
 var Version string
@@ -20,8 +18,7 @@ func version() *cobra.Command {
 	}
 
 	versionCmd.Run = func(cmd *cobra.Command, args []string) {
-		fmt.Println("NexaSDK Bridge Version: " + nexa_sdk.Version())
-		fmt.Println("NexaSDK CLI Version:    " + Version)
+		fmt.Println("Linux Arm64 Latest")
 	}
 
 	return versionCmd
