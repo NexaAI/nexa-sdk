1c31a305094beafa771a1c76c670bcca26c9039d
RemiliaForever <remilia@koumakan.cc>
2026-09-10 14:39:19 +0800

    style(android): format VLM device override
    Signed-off-by: RemiliaForever <remilia@koumakan.cc>

--- bindings/android/app/src/main/cpp/jniutils.cpp
+++ bindings/android/app/src/main/cpp/jniutils.cpp
@@ -472,8 +472,8 @@ geniex_VlmCreateInput extract_vlm_create_input(JNIEnv* env, jobject inputObj) {
     // vit_device_id : String
     fid = env->GetFieldID(cls, "vit_device_id", "Ljava/lang/String;");
     if (fid) {
-        jstr               = (jstring)env->GetObjectField(inputObj, fid);
-        out.vit_device_id  = jstr ? hold_c_str(jstring2str(env, jstr)) : nullptr;
+        jstr              = (jstring)env->GetObjectField(inputObj, fid);
+        out.vit_device_id = jstr ? hold_c_str(jstring2str(env, jstr)) : nullptr;
         if (jstr) env->DeleteLocalRef(jstr);
     }
 
